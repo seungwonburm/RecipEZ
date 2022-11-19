18f505c1dbb49669b3438b90afb3eec28a8e4649
jeetsatishv <jeetsatishv@gmail.com>
2022-11-19 14:12:33 -0500

    Added transition to activity

--- app/src/main/java/com/example/orderez/LogInPage.java
+++ app/src/main/java/com/example/orderez/LogInPage.java
@@ -2,7 +2,10 @@ package com.example.orderez;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogInPage extends AppCompatActivity {
 
@@ -10,5 +13,37 @@ public class LogInPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in_page);
+
+//        Button Login = (Button) findViewById(R.id.toLoginPagebtn);
+//        Login.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //Login the user here if correct pass and email
+//            }
+//        });
+
+        Button RegisterBtn = (Button) findViewById(R.id.registerbtn);
+        RegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Registerpage = new Intent(getApplicationContext(), RegisterPage.class);
+                startActivity(Registerpage);
+            }
+        });
+
+        Button ForgotPasswordBtn = (Button) findViewById(R.id.ForgotPasswordBtn);
+        ForgotPasswordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ForgotPasswordPage = new Intent(getApplicationContext(), ForgotPassword.class);
+                startActivity(ForgotPasswordPage);
+            }
+        });
+
+
+
     }
+
+
+
 }
--- app/src/main/java/com/example/orderez/MainActivity.java
+++ app/src/main/java/com/example/orderez/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.orderez;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button loginbtn = (Button) findViewById(R.id.toLoginPagebtn);
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent activity2Intent = new Intent(getApplicationContext(), LogInPage.class);
+                startActivity(activity2Intent);
+            }
+        });
     }
+
+
+
+
+
+
+
+
+
 }
