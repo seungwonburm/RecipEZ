541ddcae5d13b86c0b0f69e4cbe53076fcab6703
jeetsatishv <jeetsatishv@gmail.com>
2022-12-12 19:34:15 -0500

    Added recipe step functionality

---
+++ app/src/main/java/com/example/orderez/Adapters/InstructionsAdapter.java
@@ -0,0 +1,61 @@
+package com.example.orderez.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.orderez.Models.InstructionsResponse;
+import com.example.orderez.R;
+
+import java.util.List;
+
+public class InstructionsAdapter extends RecyclerView.Adapter<InstructionsViewHolder>{
+
+    Context context;
+    List<InstructionsResponse> list;
+
+    public InstructionsAdapter(Context context, List<InstructionsResponse> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public InstructionsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new InstructionsViewHolder(LayoutInflater.from(context).inflate(R.layout.list_instructions, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull InstructionsViewHolder holder, int position) {
+
+        holder.textView_instruction_name.setText(list.get(position).name);
+        holder.recycler_instructions_steps.setHasFixedSize(true);
+        holder.recycler_instructions_steps.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false));
+        InstructionStepAdapter stepAdapter = new InstructionStepAdapter(context, list.get(position).steps);
+        holder.recycler_instructions_steps.setAdapter(stepAdapter);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+}
+
+class InstructionsViewHolder extends RecyclerView.ViewHolder {
+
+    TextView textView_instruction_name;
+    RecyclerView recycler_instructions_steps;
+
+    public InstructionsViewHolder(@NonNull View itemView) {
+        super(itemView);
+        textView_instruction_name = itemView.findViewById(R.id.textView_instruction_name);
+        recycler_instructions_steps = itemView.findViewById(R.id.recycler_instructions_steps);
+    }
+}
